ec7128bb5802e68fb428a26b8374d356fe48fca4
vaibhav vatsal <vaibhavvatsalpandey01@gmail.com>
2026-02-03 12:08:21 +0530

    cp

--- node-test/fs/ind.js
+++ node-test/fs/ind.js
@@ -1,11 +1,19 @@
 const fs=require('fs');
 //fs.writeFile("./abes.txt","helina ne ki rab se fariyad",()=>{});
 
-fs.readFile("./abes.txt","utf-8",(err,res)=>{
-    if(err){
-        console.log("error",err);
-    }else{
-        console.log(res);
-    }
-});
-
+// fs.readFile("./abes.txt","utf-8",(err,res)=>{
+//     if(err){
+//         console.log("error",err);
+//     }else{
+//         console.log(res);
+//     }
+// )
+// fs.appendFileSync("./abes.txt",`aur wo rab se fariyad karne laga`);
+// fs.appendFile("./abes.txt",`\n aur wo rab se fariyad karne laga`,(err)=>{
+//     if(err){
+//         console.log("error",err);
+//     } else{
+//         console.log("file appended successfully");
+//     }
+// });       
+fs.cpSync("./abes.txt","./aa1.txt");
